287c30fdfb6a057a2cb88c43c9e1587b40062e9c
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-10-24 02:22:36 +0000

    - Cleaned up translations

--- language/en_US/wireless_lang.php
+++ language/en_US/wireless_lang.php
@@ -1,14 +1,15 @@
 <?php
 
+$lang['wireless_app_description'] = 'The Wireless Access Point app provides the engine for configuring and managning wireless network interfaces on the system.';
 $lang['wireless_app_name'] = 'Wireless Access Point';
-$lang['wireless_app_description'] = 'Wireless Access Point description...'; // FIXME
-$lang['wireless_ssid'] = 'SSID';
-$lang['wireless_ssid_invalid'] = 'SSID is invalid.';
 $lang['wireless_bridge'] = 'Bridge';
 $lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Wireless channel is invalid.';
+$lang['wireless_mode_invalid'] = 'Wireless mode is invalid.';
 $lang['wireless_mode'] = 'Mode';
 $lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_ssid_invalid'] = 'SSID is invalid.';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
 $lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
 $lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
-$lang['wireless_recommended'] = 'Recommended';
-$lang['wireless_wep'] = 'WEP';

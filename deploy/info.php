2936132b9d33569ef22def4b1aace1a0f3bd6da5
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-05-23 20:20:45 +0000

    - Fixed nslcd daemon description [fixed tracker #1153]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'wireless';
-$app['version'] = '1.4.20';
+$app['version'] = '1.4.36';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';

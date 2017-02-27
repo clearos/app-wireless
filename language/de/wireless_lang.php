1c755862ac2c3107a54c8c4cbda54028a8b2ebcd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Der Wireless Access Point App bietet den Motor für die Konfiguration und Verwaltung von Wireless-Netzwerk-Schnittstellen im System.';
+$lang['wireless_app_name'] = 'WLAN-Zugangspunkt';
+$lang['wireless_bridge'] = 'Brücke';
+$lang['wireless_channel'] = 'Kanal';
+$lang['wireless_channel_invalid'] = 'Wireless-Kanal ist ungültig.';
+$lang['wireless_mode_invalid'] = 'Wireless-Modus ist ungültig.';
+$lang['wireless_mode'] = 'Modus';
+$lang['wireless_passphrase'] = 'Paßphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID ist ungültig.';
+$lang['wireless_interface'] = 'Drahtlose Schnittstelle';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA-Infrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Ein WLAN-Passwort ist ungültig';

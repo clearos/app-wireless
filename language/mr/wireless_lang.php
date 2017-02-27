7ec992a0435b2cd12df05367ba73aed43a9df179
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'वायरलेस प्रवेश बिंदू अनुप्रयोग संरचीत व प्रणाली वायरलेस नेटवर्क संवाद व्यवस्थापित करण्यासाठी इंजिन उपलब्ध आहे.';
+$lang['wireless_app_name'] = 'वायरलेस प्रवेश बिंदू';
+$lang['wireless_bridge'] = 'ब्रिज';
+$lang['wireless_channel'] = 'चॅनेल';
+$lang['wireless_channel_invalid'] = 'वायरलेस चॅनेल अवैध आहे.';
+$lang['wireless_mode_invalid'] = 'वायरलेस मोड अवैध आहे.';
+$lang['wireless_mode'] = 'मोड';
+$lang['wireless_passphrase'] = 'सांकेतिक वाक्यांश';
+$lang['wireless_radius_secret'] = 'RADIUS सामायिक गुप्त';
+$lang['wireless_ssid_invalid'] = 'SSID अवैध आहे.';
+$lang['wireless_interface'] = 'वायरलेस इंटरफेस';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA इन्फ्रास्ट्रक्चर';
+$lang['wireless_wpa_preshared_key'] = 'WPA पूर्व-सामायिक की';
+$lang['wireless_passphrase_invalid'] = 'वायरलेस सांकेतिक वाक्यांश अवैध आहे';

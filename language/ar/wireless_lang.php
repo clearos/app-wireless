4acfe1f4e6a454f3994630c6b5e6cbc77753481c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ويوفر التطبيق نقطة وصول لاسلكية المحرك لتكوين وإدارة واجهات شبكة لاسلكية على النظام.';
+$lang['wireless_app_name'] = 'نقطة الوصول اللاسلكي';
+$lang['wireless_bridge'] = 'جسر';
+$lang['wireless_channel'] = 'قناة';
+$lang['wireless_channel_invalid'] = 'قناة لاسلكية غير صالحة.';
+$lang['wireless_mode_invalid'] = 'وضع اللاسلكية غير صالح.';
+$lang['wireless_mode'] = 'الوضع';
+$lang['wireless_passphrase'] = 'مرور';
+$lang['wireless_radius_secret'] = 'سر المشتركة RADIUS';
+$lang['wireless_ssid_invalid'] = 'SSID غير صالح.';
+$lang['wireless_interface'] = 'واجهة لاسلكية';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA البنية التحتية';
+$lang['wireless_wpa_preshared_key'] = 'WPA مفتاح مشترك مسبقا';
+$lang['wireless_passphrase_invalid'] = 'عبارة المرور اللاسلكية غير صالح';

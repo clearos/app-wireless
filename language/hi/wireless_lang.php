6b98e2f7310838cc37d98dbcbbf7d6e047b909f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'वायरलेस एक्सेस प्वाइंट एप्लिकेशन विन्यस्त और सिस्टम पर वायरलेस नेटवर्क इंटरफेस के प्रबंधन के लिए इंजन प्रदान करता है।';
+$lang['wireless_app_name'] = 'बेतार संग्रहण बिन्दू';
+$lang['wireless_bridge'] = 'पुल';
+$lang['wireless_channel'] = 'चैनल';
+$lang['wireless_channel_invalid'] = 'वायरलेस चैनल अमान्य है।';
+$lang['wireless_mode_invalid'] = 'वायरलेस मोड अमान्य है।';
+$lang['wireless_mode'] = 'मोड';
+$lang['wireless_passphrase'] = 'पदबंध';
+$lang['wireless_radius_secret'] = 'त्रिज्या साझा रहस्य';
+$lang['wireless_ssid_invalid'] = 'SSID अमान्य है।';
+$lang['wireless_interface'] = 'वायरलेस इंटरफ़ेस';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'डबल्यु इन्फ्रास्ट्रक्चर';
+$lang['wireless_wpa_preshared_key'] = 'डबल्युपीए पूर्व साझा कुंजी';
+$lang['wireless_passphrase_invalid'] = 'वायरलेस पदबंध अमान्य है';

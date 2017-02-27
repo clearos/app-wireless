33844271c5a652bca8dd587bbb9be91477b48876
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Ang Wireless Access Point app ay nagbibigay ng engine para sa pag-configure at pamamahala wireless network interface sa sistema.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'tulay';
+$lang['wireless_channel'] = 'channel';
+$lang['wireless_channel_invalid'] = 'Wireless channel ay hindi wasto.';
+$lang['wireless_mode_invalid'] = 'Wireless mode ay hindi wasto.';
+$lang['wireless_mode'] = 'paraan';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID ay hindi wasto.';
+$lang['wireless_interface'] = 'wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase ay Hindi wastong';

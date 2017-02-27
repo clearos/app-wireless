9541ffc7f2992c4dc050fac0f9f031c46f14b696
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The Wireless Access Point ծրագիրը ապահովում է շարժիչը օպտիմալ եւ կառավարման անլար ցանցի ինտերֆեյս համակարգի:';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'կամուրջ';
+$lang['wireless_channel'] = 'ջրանցք';
+$lang['wireless_channel_invalid'] = 'Wireless ալիքը անվավեր:';
+$lang['wireless_mode_invalid'] = 'Wireless ռեժիմ է անվավեր:';
+$lang['wireless_mode'] = 'ռեժիմ';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID է անվավեր:';
+$lang['wireless_interface'] = 'Wireless Ինտերֆեյս';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-կիսում Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase անվավեր';

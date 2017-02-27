dd11a0dab2ac2b0359e278ec19d075d0ea406040
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Det trådløse tilgangspunktet app gir motoren for å konfigurere og administrere trådløse nettverksgrensesnitt på systemet.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bro';
+$lang['wireless_channel'] = 'Kanal';
+$lang['wireless_channel_invalid'] = 'Trådløs kanal er ugyldig.';
+$lang['wireless_mode_invalid'] = 'Trådløst modus er ugyldig.';
+$lang['wireless_mode'] = 'Modus';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID er ugyldig.';
+$lang['wireless_interface'] = 'Trådløst grensesnitt';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Trådløst Passordet er ugyldig';

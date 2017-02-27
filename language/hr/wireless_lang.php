1dfcbb381c387d5ba0307a103e2af80e6efa0dd3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:49:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Aplikacija Wireless Access Point omogućuje motor za konfiguriranje i upravljanje bežičnim mrežnih sučelja na sustavu.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Most';
+$lang['wireless_channel'] = 'Kanal';
+$lang['wireless_channel_invalid'] = 'Bežični kanala nije valjan.';
+$lang['wireless_mode_invalid'] = 'Bežični način rada je nevažeći.';
+$lang['wireless_mode'] = 'način';
+$lang['wireless_passphrase'] = 'zaporka';
+$lang['wireless_radius_secret'] = 'RADIUS zajednički tajni';
+$lang['wireless_ssid_invalid'] = 'SSID je nevažeći.';
+$lang['wireless_interface'] = 'Bežični sučelje';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktura';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Bežični Lozinka nije valjana';

dbd621721935dcfbeeaf9e932b1b708be52a7e6d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:00:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The Alailowaya Access Point app pese awọn engine fun leto ati idari alailowaya nẹtiwọki atọkun lori awọn eto.';
+$lang['wireless_app_name'] = 'Alailowaya Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'ikanni';
+$lang['wireless_channel_invalid'] = 'Alailowaya ikanni ti wa ni invalid.';
+$lang['wireless_mode_invalid'] = 'Alailowaya mode ti wa ni invalid.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'kukuru';
+$lang['wireless_radius_secret'] = 'Rediosi Pipin Secret';
+$lang['wireless_ssid_invalid'] = 'SSID ni invalid.';
+$lang['wireless_interface'] = 'alailowaya Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA amayederun';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-pín Key';
+$lang['wireless_passphrase_invalid'] = 'Alailowaya kukuru ni Invalid';

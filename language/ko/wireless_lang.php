f54de3f5b664365a8a74cc3602ef149094a8db5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = '무선 액세스 포인트 앱 구성 및 시스템에 무선 네트워크 인터페이스를 관리하기위한 엔진을 제공합니다.';
+$lang['wireless_app_name'] = '무선 액세스 포인트';
+$lang['wireless_bridge'] = '다리';
+$lang['wireless_channel'] = '채널';
+$lang['wireless_channel_invalid'] = '무선 채널이 잘못되었습니다.';
+$lang['wireless_mode_invalid'] = '무선 모드가 잘못되었습니다.';
+$lang['wireless_mode'] = '방법';
+$lang['wireless_passphrase'] = '암호';
+$lang['wireless_radius_secret'] = 'RADIUS 공유 비밀';
+$lang['wireless_ssid_invalid'] = 'SSID가 잘못되었습니다.';
+$lang['wireless_interface'] = '무선 인터페이스';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA 인프라';
+$lang['wireless_wpa_preshared_key'] = 'WPA 사전 공유 키';
+$lang['wireless_passphrase_invalid'] = '무선 암호 구가 잘못되었습니다';

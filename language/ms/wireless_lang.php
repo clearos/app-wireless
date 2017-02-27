3bd9dabc6dbe2ad518175e1ef445e71df4033500
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Aplikasi Wireless Access Point menyediakan enjin untuk mengkonfigurasi dan menguruskan muka rangkaian wayarles pada sistem.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Jambatan';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'saluran wayarles tidak sah.';
+$lang['wireless_mode_invalid'] = 'mod Wireless adalah tidak sah.';
+$lang['wireless_mode'] = 'mod';
+$lang['wireless_passphrase'] = 'Frasa laluan';
+$lang['wireless_radius_secret'] = 'RADIUS Secret Shared';
+$lang['wireless_ssid_invalid'] = 'SSID tidak sah.';
+$lang['wireless_interface'] = 'Antara Muka Tanpa wayar';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA prakongsi Key';
+$lang['wireless_passphrase_invalid'] = 'Frasa laluan wayarles adalah sah';

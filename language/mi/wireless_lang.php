73dd71498eac9e22f9243e6434b18979dc221e90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'whakarato te taupānga Ahokore Access Point te pūkaha mo te whirihora me te whakahaere atanga whatunga ahokore i runga i te pūnaha.';
+$lang['wireless_app_name'] = 'Point Access Ahokore';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'He muhu hongere Ahokore.';
+$lang['wireless_mode_invalid'] = 'He muhu aratau Ahokore.';
+$lang['wireless_mode'] = 'aratau';
+$lang['wireless_passphrase'] = 'kīangahipa';
+$lang['wireless_radius_secret'] = 'RADIUS Tiri Secret';
+$lang['wireless_ssid_invalid'] = 'He muhu SSID.';
+$lang['wireless_interface'] = 'Atanga ahokore';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'TūTW';
+$lang['wireless_wpa_infrastructure'] = 'UAPK Hanganga';
+$lang['wireless_wpa_preshared_key'] = 'Key UAPK Pre-ngā';
+$lang['wireless_passphrase_invalid'] = 'He muhu Kīangahipa ahokore';

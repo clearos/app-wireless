c6a1f00c59d239362c1c2fc00b2592e617bd0c96
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:59:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Mae&#39;r app Di-wifr pwynt mynediad yn darparu&#39;r peiriant ar gyfer ffurfweddu a rheoli rhyngwynebau rhwydwaith di-wifr ar y system.';
+$lang['wireless_app_name'] = 'Pwynt Mynediad Di-wifr';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'sianel';
+$lang['wireless_channel_invalid'] = 'sianel di-wifr yn annilys.';
+$lang['wireless_mode_invalid'] = 'modd di-wifr yn annilys.';
+$lang['wireless_mode'] = 'modd';
+$lang['wireless_passphrase'] = 'ymadrodd cyfrin';
+$lang['wireless_radius_secret'] = 'RADIUS Rennir Secret';
+$lang['wireless_ssid_invalid'] = 'SSID yn annilys.';
+$lang['wireless_interface'] = 'Di-wifr Rhyngwyneb';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'Isadeiledd WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Cyn-rennir allweddol';
+$lang['wireless_passphrase_invalid'] = 'Cyfrinosodiad Di-wifr yn annilys';

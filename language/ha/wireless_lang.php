4768c53415a14152d949c647c72845b5bbdc5582
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The Wireless Access Point app bayar da engine for harhadawa da manajan mara waya na cibiyar sadarwa musaya a kan tsarin.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Wireless channel ba daidai ba ne.';
+$lang['wireless_mode_invalid'] = 'Wireless hanya ba daidai ba ne.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Radius Shared Asirin';
+$lang['wireless_ssid_invalid'] = 'SSID ba daidai ba ne.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Lantarki';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase ne mara inganci';

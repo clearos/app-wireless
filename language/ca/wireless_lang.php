1c79e3f5b42255e9f34460804dba9f5d80e108cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:49:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'L&#39;aplicació Wireless Access Point proporciona el motor per configurar i administrar les interfícies de xarxa sense fils en el sistema.';
+$lang['wireless_app_name'] = 'Punt d&#39;accés sense fil';
+$lang['wireless_bridge'] = 'pont';
+$lang['wireless_channel'] = 'canal';
+$lang['wireless_channel_invalid'] = 'canal sense fil no és vàlid.';
+$lang['wireless_mode_invalid'] = 'La manera sense fil no és vàlid.';
+$lang['wireless_mode'] = 'manera';
+$lang['wireless_passphrase'] = 'contrasenya';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID no és vàlid.';
+$lang['wireless_interface'] = 'interfície sense fil';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'infraestructura WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Frase de wi-fi no vàlida';

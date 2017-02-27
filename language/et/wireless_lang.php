1dfec250803f2246b269af7c6517ee53b98e6104
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:50:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point app annab mootori seadistamise ja haldamise traadita võrgu liidesed süsteemi.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'sild';
+$lang['wireless_channel'] = 'kanal';
+$lang['wireless_channel_invalid'] = 'Traadita kanal on kehtetu.';
+$lang['wireless_mode_invalid'] = 'Traadita režiim ei sobi.';
+$lang['wireless_mode'] = 'viis';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Jagatud saladus';
+$lang['wireless_ssid_invalid'] = 'SSID on kehtetu.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA infrastruktuuri';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Traadita Passphrase on vale';

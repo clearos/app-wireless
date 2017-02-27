e773ce16323fdb818d70f61a7de5e104219a5f54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Il-app Wireless Access Point jipprovdi l-magna għall-konfigurazzjoni u l-ġestjoni interfaces tan-netwerk wireless fuq is-sistema.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'pont';
+$lang['wireless_channel'] = 'kanal';
+$lang['wireless_channel_invalid'] = 'Wireless kanal huwa invalidu.';
+$lang['wireless_mode_invalid'] = 'Wireless modalità huwa invalidu.';
+$lang['wireless_mode'] = 'modalità';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'F&#39;radda Maqsuma Sigriet';
+$lang['wireless_ssid_invalid'] = 'SSID huwa invalidu.';
+$lang['wireless_interface'] = 'wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruttura';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-maqsuma Ewlenin';
+$lang['wireless_passphrase_invalid'] = 'Wireless passphrase huwa Invalid';

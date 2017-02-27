01c83179e57a9e754d881ecf099520c88f5e8b64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Утасгүй сүлжээний холболтын цэг апп-г тохируулах нь таны систем болон утасгүй сүлжээний интерфэйсүүдийн удирдах хөдөлгүүртэй болно.';
+$lang['wireless_app_name'] = 'Утасгүй сүлжээний холболтын цэг';
+$lang['wireless_bridge'] = 'гүүр';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Утасгүй суваг хүчин төгөлдөр бус байна.';
+$lang['wireless_mode_invalid'] = 'Утасгүй сүлжээний горим хүчин төгөлдөр бус байна.';
+$lang['wireless_mode'] = 'Горим';
+$lang['wireless_passphrase'] = 'Нэвтрэх';
+$lang['wireless_radius_secret'] = 'RADIUS хуваалцсан нууц';
+$lang['wireless_ssid_invalid'] = 'SSID нь хүчин төгөлдөр бус байна.';
+$lang['wireless_interface'] = 'Утасгүй сүлжээний интерфэйс';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Дэд бүтэц';
+$lang['wireless_wpa_preshared_key'] = 'Түлхүүр WPA Урьдчилсан хуваалцсан';
+$lang['wireless_passphrase_invalid'] = 'Утасгүй Нэвтрэх үг буруу байна';

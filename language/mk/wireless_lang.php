7fe2ffcefcdb4036681601971c964d5f459dd588
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'стан безжичната точка за пристап овозможува на моторот за конфигурирање и управување со безжичната мрежа интерфејс на системот.';
+$lang['wireless_app_name'] = 'Безжична пристапна точка';
+$lang['wireless_bridge'] = 'мост';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'Безжичен канал не е валиден.';
+$lang['wireless_mode_invalid'] = 'Безжичен начин на работа не е валиден.';
+$lang['wireless_mode'] = 'режим';
+$lang['wireless_passphrase'] = 'Тајната фраза';
+$lang['wireless_radius_secret'] = 'Радиус заедничка тајна';
+$lang['wireless_ssid_invalid'] = 'SSID е валиден.';
+$lang['wireless_interface'] = 'безжичен интерфејс';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA инфраструктура';
+$lang['wireless_wpa_preshared_key'] = 'WPA-пред-споделени клуч';
+$lang['wireless_passphrase_invalid'] = 'Безжична Тајната фраза не е валиден';

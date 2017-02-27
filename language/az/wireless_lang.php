0e63f6603b687e051e99e071b0b7dc7a9b07161f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point app konfiqurasiya və sistemi simsiz şəbəkə interfeysləri idarə olunması üçün mühərrik təmin edir.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'körpü';
+$lang['wireless_channel'] = 'kanal';
+$lang['wireless_channel_invalid'] = 'Wireless kanal yanlışdır.';
+$lang['wireless_mode_invalid'] = 'Wireless rejimi yanlışdır.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID yanlışdır.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA İnfrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Key Pre-paylaşdı';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase yanlışdır';

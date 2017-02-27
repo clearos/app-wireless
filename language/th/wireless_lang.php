5864a971f10c7d7ca186fa44f46c7b424580d25b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'แอปพลิเค Wireless Access Point ให้เครื่องยนต์สำหรับการกำหนดค่าและการจัดการการเชื่อมต่อเครือข่ายไร้สายในระบบ';
+$lang['wireless_app_name'] = 'Access Point Wireless';
+$lang['wireless_bridge'] = 'สะพาน';
+$lang['wireless_channel'] = 'ช่อง';
+$lang['wireless_channel_invalid'] = 'ช่องสัญญาณไร้สายไม่ถูกต้อง';
+$lang['wireless_mode_invalid'] = 'โหมดไร้สายที่ไม่ถูกต้อง';
+$lang['wireless_mode'] = 'โหมด';
+$lang['wireless_passphrase'] = 'ข้อความรหัสผ่าน';
+$lang['wireless_radius_secret'] = 'RADIUS ลับร่วมกัน';
+$lang['wireless_ssid_invalid'] = 'SSID ไม่ถูกต้อง';
+$lang['wireless_interface'] = 'การเชื่อมต่อแบบไร้สาย';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'โครงสร้างพื้นฐานของ WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'รหัสผ่านไม่ถูกต้องไร้สาย';

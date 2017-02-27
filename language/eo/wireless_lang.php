13c3c49880651250912db334f25cd7b3f882f293
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:50:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'La Wireless Access Point app provizas la motoron por agordi kaj administri sendrata reto interfacoj sur la sistemo.';
+$lang['wireless_app_name'] = 'Sendrata Access Point';
+$lang['wireless_bridge'] = 'ponto';
+$lang['wireless_channel'] = 'kanalo';
+$lang['wireless_channel_invalid'] = 'Sendrata kanalo estas nevalida.';
+$lang['wireless_mode_invalid'] = 'Sendrata modo estas nevalida.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Komuna Sekreta';
+$lang['wireless_ssid_invalid'] = 'SSID estas nevalida.';
+$lang['wireless_interface'] = 'sendrata Interfaco';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastrukturo';
+$lang['wireless_wpa_preshared_key'] = 'WPA Antaŭ- dividitaj Ŝlosilo';
+$lang['wireless_passphrase_invalid'] = 'Sendrata passphrase estas Nevalida';

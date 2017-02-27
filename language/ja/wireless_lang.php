e561dd956c7b60cc601edc7aaf0507d8bc1a4f21
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ワイヤレスアクセスポイントアプリは、システム上の無線ネットワークインターフェースを設定し、管理するためのエンジンを提供します。';
+$lang['wireless_app_name'] = 'ワイヤレスアクセスポイント';
+$lang['wireless_bridge'] = 'ブリッジ';
+$lang['wireless_channel'] = 'チャネル';
+$lang['wireless_channel_invalid'] = '無線チャネルは無効です。';
+$lang['wireless_mode_invalid'] = 'ワイヤレスモードは無効です。';
+$lang['wireless_mode'] = 'モード';
+$lang['wireless_passphrase'] = 'パスフレーズ';
+$lang['wireless_radius_secret'] = 'RADIUS共有シークレット';
+$lang['wireless_ssid_invalid'] = 'SSIDは無効です。';
+$lang['wireless_interface'] = 'ワイヤレスインターフェース';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPAのインフラストラクチャ';
+$lang['wireless_wpa_preshared_key'] = 'WPA事前共有キー';
+$lang['wireless_passphrase_invalid'] = 'ワイヤレスパスフレーズが無効です';

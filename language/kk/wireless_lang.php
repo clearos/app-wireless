755d2fdbcf3b388e190a5a7cb9d0c08a5577963b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Сымсыз Access Point App жүйесі сымсыз желі интерфейстерін теңшеу және басқару үшін қозғалтқыш қамтамасыз етеді.';
+$lang['wireless_app_name'] = 'Сымсыз кіру нүктесі';
+$lang['wireless_bridge'] = 'көпір';
+$lang['wireless_channel'] = 'арна';
+$lang['wireless_channel_invalid'] = 'Сымсыз арна жарамсыз болып табылады.';
+$lang['wireless_mode_invalid'] = 'Сымсыз режим жарамсыз болып табылады.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Кілттізбегі';
+$lang['wireless_radius_secret'] = 'RADIUS Ортақ Secret';
+$lang['wireless_ssid_invalid'] = 'SSID жарамсыз болып табылады.';
+$lang['wireless_interface'] = 'Сымсыз интерфейс';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Инфрақұрылым';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Сымсыз Кілттізбегі жарамсыз';

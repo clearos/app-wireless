f9174290774411f03c684c14185a974e450ec4db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point aplikasi menyediakan mesin untuk mengkonfigurasi dan mengelola antarmuka jaringan nirkabel pada sistem.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Jembatan';
+$lang['wireless_channel'] = 'Saluran';
+$lang['wireless_channel_invalid'] = 'saluran nirkabel tidak valid.';
+$lang['wireless_mode_invalid'] = 'mode nirkabel tidak valid.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID tidak valid.';
+$lang['wireless_interface'] = 'wireless Antarmuka';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Passphrase nirkabel tidak valid';

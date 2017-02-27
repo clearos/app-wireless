49bd59240c1fb85aa19693395eeca48d10ccd881
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:50:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'L&#39;application Wireless Access Point fournit le moteur pour la configuration et la gestion des interfaces de réseau sans fil sur le système.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Pont';
+$lang['wireless_channel'] = 'Canal';
+$lang['wireless_channel_invalid'] = 'canal sans fil est invalide.';
+$lang['wireless_mode_invalid'] = 'Mode sans fil est invalide.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS secret partagé';
+$lang['wireless_ssid_invalid'] = 'SSID est invalide.';
+$lang['wireless_interface'] = 'Interface sans fil';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Passphrase sans fil est non valide';

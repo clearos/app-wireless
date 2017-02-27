707ba724a7b82e7b68d2c1947e14285722e4d5cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ਵਾਇਰਲੈੱਸ ਐਕਸੈੱਸ ਪੁਆਇੰਟ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਸੰਰਚਿਤ ਅਤੇ ਸਿਸਟਮ ਤੇ ਬੇਤਾਰ ਨੈੱਟਵਰਕ ਇੰਟਰਫੇਸ ਦਾ ਪ੍ਰਬੰਧ ਕਰਨ ਲਈ ਇੰਜਣ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['wireless_app_name'] = 'ਵਾਇਰਲੈੱਸ ਐਕਸੈੱਸ ਪੁਆਇੰਟ';
+$lang['wireless_bridge'] = 'ਪੁਲ';
+$lang['wireless_channel'] = 'ਚੈਨਲ';
+$lang['wireless_channel_invalid'] = 'ਵਾਇਰਲੈੱਸ ਚੈਨਲ ਗਲਤ ਹੈ.';
+$lang['wireless_mode_invalid'] = 'ਵਾਇਰਲੈੱਸ ਮੋਡ ਗਲਤ ਹੈ.';
+$lang['wireless_mode'] = 'ਮੋਡ';
+$lang['wireless_passphrase'] = 'ਪ੍ਹੈਰਾ';
+$lang['wireless_radius_secret'] = 'RADIUS ਸ਼ੇਅਰ ਗੁਪਤ';
+$lang['wireless_ssid_invalid'] = 'SSID ਗਲਤ ਹੈ.';
+$lang['wireless_interface'] = 'ਵਾਇਰਲੈੱਸ ਇੰਟਰਫੇਸ';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA ਬੁਨਿਆਦੀ';
+$lang['wireless_wpa_preshared_key'] = 'WPA ਪ੍ਰੀ-ਸ਼ੇਅਰ ਕੁੰਜੀ';
+$lang['wireless_passphrase_invalid'] = 'ਵਾਇਰਲੈੱਸ ਪ੍ਹੈਰਾ ਗਲਤ ਹੈ';

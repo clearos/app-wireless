72bfd271216ee5b627cd1629f842df9a6f611505
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'برنامه نقطه دسترسی بی سیم برای پیکربندی و مدیریت رابط های شبکه بی سیم در سیستم فراهم می کند موتور.';
+$lang['wireless_app_name'] = 'نقطه دسترسی بی سیم';
+$lang['wireless_bridge'] = 'پل';
+$lang['wireless_channel'] = 'کانال';
+$lang['wireless_channel_invalid'] = 'کانال رادیویی نامعتبر است.';
+$lang['wireless_mode_invalid'] = 'حالت های بی سیم نامعتبر است.';
+$lang['wireless_mode'] = 'حالت';
+$lang['wireless_passphrase'] = 'عبارت عبور';
+$lang['wireless_radius_secret'] = 'RADIUS راز به اشتراک گذاشته شده';
+$lang['wireless_ssid_invalid'] = 'SSID نامعتبر است.';
+$lang['wireless_interface'] = 'رابط بی سیم';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'زیرساخت WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA کلید از پیش مشترک';
+$lang['wireless_passphrase_invalid'] = 'کلمه عبور بی سیم نامعتبر است';

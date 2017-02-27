45963e9077dff79fbbfd7fa375eb5291639feaf6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:49:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'အဆိုပါကြိုးမဲ့ Access Point app အားစနစ်အပေါ် wireless network ကို interfaces configure လုပ်ပြီးစီမံခန့်ခွဲမှုအတွက်အင်ဂျင်ပေးစွမ်းသည်။';
+$lang['wireless_app_name'] = 'ကြိုးမဲ့ Access Point';
+$lang['wireless_bridge'] = 'တံတား';
+$lang['wireless_channel'] = 'channel ကို';
+$lang['wireless_channel_invalid'] = 'ကြိုးမဲ့ရုပ်သံလိုင်းမမှန်ကန်ဘူး။';
+$lang['wireless_mode_invalid'] = 'ကြိုးမဲ့ mode ကိုမမှန်ကန်ဘူး။';
+$lang['wireless_mode'] = 'mode ကို';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret ကို';
+$lang['wireless_ssid_invalid'] = 'SSID မမှန်ကန်ဘူး။';
+$lang['wireless_interface'] = 'ကြိုးမဲ့ Interface ကို';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA အခြေခံအဆောက်အအုံ';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key ကို';
+$lang['wireless_passphrase_invalid'] = 'ကြိုးမဲ့ Passphrase မှားနေသောဖြစ်ပါသည်';

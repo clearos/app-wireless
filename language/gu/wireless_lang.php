3ad568028e042ef17a9322ce4b95557ebd881d78
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'વાયરલેસ એક્સેસ પોઇન્ટ એપ્લિકેશન રૂપરેખાંકિત અને સિસ્ટમ પર વાયરલેસ નેટવર્ક ઇન્ટરફેસોને વ્યવસ્થા કરવા માટે એન્જિન પૂરી પાડે છે.';
+$lang['wireless_app_name'] = 'વાયરલેસ એક્સેસ પોઇન્ટ';
+$lang['wireless_bridge'] = 'બ્રિજ';
+$lang['wireless_channel'] = 'ચેનલ';
+$lang['wireless_channel_invalid'] = 'વાયરલેસ ચેનલ અમાન્ય છે.';
+$lang['wireless_mode_invalid'] = 'વાયરલેસ સ્થિતિ અમાન્ય છે.';
+$lang['wireless_mode'] = 'સ્થિતિ';
+$lang['wireless_passphrase'] = 'પાસફ્રેઝ';
+$lang['wireless_radius_secret'] = 'રેડિયસ વહેંચાયેલ ગુપ્ત';
+$lang['wireless_ssid_invalid'] = 'SSID અમાન્ય છે.';
+$lang['wireless_interface'] = 'વાયરલેસ ઈન્ટરફેસ';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'ડબલ્યુપીએ ઈન્ફ્રાસ્ટ્રક્ચર';
+$lang['wireless_wpa_preshared_key'] = 'ડબલ્યુપીએ પૂર્વ-શેર કરેલી કી';
+$lang['wireless_passphrase_invalid'] = 'વાયરલેસ પાસફ્રેઝ અમાન્ય છે';

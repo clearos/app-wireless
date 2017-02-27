773dd891c795be462c8f23218c04a6802175ae9f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Access Point App Engine et providet et in Wireless Wireless Vestibulum et administrandi ratio a network interfaces.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'pretium';
+$lang['wireless_channel_invalid'] = 'Wireless alveo, invalidum est.';
+$lang['wireless_mode_invalid'] = 'Wireless ratio est invalidum.';
+$lang['wireless_mode'] = 'Modus';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'Secret RADIUS Shared';
+$lang['wireless_ssid_invalid'] = 'SSID est invalidum.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'Infrastructure WPA';
+$lang['wireless_wpa_preshared_key'] = 'Pre-Key WPA shared';
+$lang['wireless_passphrase_invalid'] = 'Wireless passphrase invalida';

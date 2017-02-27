c476a06a68b3d8ddae42719875fbe40d05e6c75b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:57:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The app e Wireless Access Point o fana ka enjene bakeng Hlophisa le laola netweke likarolo waelese tsamaisong eo.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Wireless kanale ha o sebetse.';
+$lang['wireless_mode_invalid'] = 'screen reader mode Wireless ha o sebetse.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'tselaitshupo';
+$lang['wireless_radius_secret'] = 'RADIUS ba ile ba kopanela Lekunutu';
+$lang['wireless_ssid_invalid'] = 'SSID ha o sebetse.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'bao mat wep';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-ba ile ba kopanela Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless tselaitshupo ha o sebetse';

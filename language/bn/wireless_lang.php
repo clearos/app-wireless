ea5bc995c00fba4085a4a50fc0c45aaa58ad5770
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ওয়্যারলেস অ্যাকসেস পয়েন্ট অ্যাপ্লিকেশন কনফিগার করার এবং সিস্টেমে বেতার নেটওয়ার্ক ইন্টারফেসগুলো অনুসন্ধান পরিচালনার জন্য ইঞ্জিন উপলব্ধ.';
+$lang['wireless_app_name'] = 'ওয়্যারলেস অ্যাকসেস পয়েন্ট';
+$lang['wireless_bridge'] = 'সেতু';
+$lang['wireless_channel'] = 'চ্যানেল';
+$lang['wireless_channel_invalid'] = 'ওয়্যারলেস চ্যানেল অবৈধ.';
+$lang['wireless_mode_invalid'] = 'ওয়্যারলেস মোড অবৈধ.';
+$lang['wireless_mode'] = 'মোড';
+$lang['wireless_passphrase'] = 'পাসফ্রেজ';
+$lang['wireless_radius_secret'] = 'ব্যাসার্ধ শেয়ারড সিক্রেট';
+$lang['wireless_ssid_invalid'] = 'এসএসআইডি অবৈধ.';
+$lang['wireless_interface'] = 'ওয়্যারলেস ইন্টারফেস';
+$lang['wireless_ssid'] = 'এসএসআইডি';
+$lang['wireless_wep'] = 'করুন WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA ইনফ্রাস্ট্রাকচার';
+$lang['wireless_wpa_preshared_key'] = 'WPA প্রাক অংশীদারিকৃত কী';
+$lang['wireless_passphrase_invalid'] = 'ওয়্যারলেস পাসফ্রেজ অবৈধ';

a38c093af0af7678e18ca5386219dbe3318d3a9f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'వైర్లెస్ యాక్సెస్ పాయింట్ అనువర్తనం ఆకృతీకరించుట మరియు వ్యవస్థ వైర్లెస్ నెట్వర్క్ అనుసంధానాలు నిర్వహణా ఇంజిన్ అందిస్తుంది.';
+$lang['wireless_app_name'] = 'వైర్లెస్ యాక్సెస్ పాయింట్';
+$lang['wireless_bridge'] = 'బ్రిడ్జ్';
+$lang['wireless_channel'] = 'ఛానల్';
+$lang['wireless_channel_invalid'] = 'వైర్లెస్ ఛానల్ చెల్లదు.';
+$lang['wireless_mode_invalid'] = 'వైర్లెస్ మోడ్ చెల్లదు.';
+$lang['wireless_mode'] = 'మోడ్';
+$lang['wireless_passphrase'] = 'పాస్ఫ్రేజ్';
+$lang['wireless_radius_secret'] = 'వ్యాసార్థం భాగస్వామ్య రహస్యాన్ని';
+$lang['wireless_ssid_invalid'] = 'SSID చెల్లుబాటు కాదు.';
+$lang['wireless_interface'] = 'వైర్లెస్ ఇంటర్ఫేస్';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA ఇన్ఫ్రాస్ట్రక్చర్';
+$lang['wireless_wpa_preshared_key'] = 'WPA పూర్వ భాగస్వామ్య కీ';
+$lang['wireless_passphrase_invalid'] = 'వైర్లెస్ పాస్ఫ్రేజ్ చెల్లదు';

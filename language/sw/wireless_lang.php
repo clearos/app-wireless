12a2824112eab9d713541650b3c40d42669e890a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'programu Wireless Access Point hutoa injini kwa ajili ya Configuring na kusimamia mtandao interfaces wireless juu ya mfumo.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Daraja';
+$lang['wireless_channel'] = 'channel';
+$lang['wireless_channel_invalid'] = 'Wireless channel ni batili.';
+$lang['wireless_mode_invalid'] = 'mode Wireless ni batili.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Pamoja Siri';
+$lang['wireless_ssid_invalid'] = 'SSID ni batili.';
+$lang['wireless_interface'] = 'wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Miundombinu';
+$lang['wireless_wpa_preshared_key'] = 'WPA Kabla ya pamoja Muhimu';
+$lang['wireless_passphrase_invalid'] = 'Wireless Kaulisiri ni batili';

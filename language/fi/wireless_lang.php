05e4f844a7a689628189f8e7d61d5109904157c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:50:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point sovellus tarjoaa moottorin konfigurointiin ja hallintaan langattoman verkon rajapintojen järjestelmään.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Silta';
+$lang['wireless_channel'] = 'kanava';
+$lang['wireless_channel_invalid'] = 'Langaton kanava on virheellinen.';
+$lang['wireless_mode_invalid'] = 'Langaton tila ei kelpaa.';
+$lang['wireless_mode'] = 'tila';
+$lang['wireless_passphrase'] = 'Tunnuslause';
+$lang['wireless_radius_secret'] = 'RADIUS jaettu salaisuus';
+$lang['wireless_ssid_invalid'] = 'SSID on virheellinen.';
+$lang['wireless_interface'] = 'Langaton liitäntä';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Langaton Tunnuslause on virheellinen';

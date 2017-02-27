46702e291d561ae8e658efc9c972c97c68b86266
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Дадатак Бесправадная кропка доступу забяспечвае механізм для налады і кіравання сеткавымі інтэрфейсамі бесправадной сістэмы.';
+$lang['wireless_app_name'] = 'Бесправадная кропка доступу';
+$lang['wireless_bridge'] = 'мост';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'Бесправадной канал з&#39;яўляецца несапраўдным.';
+$lang['wireless_mode_invalid'] = 'Бесправадной рэжым з&#39;яўляецца недапушчальным.';
+$lang['wireless_mode'] = 'рэжым';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID з&#39;яўляецца несапраўдным.';
+$lang['wireless_interface'] = 'бесправадны інтэрфейс';
+$lang['wireless_ssid'] = 'ідэнтыфікатар SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Інфраструктура';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Бесправадной Passphrase недапушчальны';

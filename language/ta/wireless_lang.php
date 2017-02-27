f35ec6e5f32ac4f330a1f7ffaa1518e2541d0755
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'வயர்லெஸ் அணுகல் புள்ளி பயன்பாட்டை கட்டமைக்கும் மற்றும் அமைப்பு மீது வயர்லெஸ் நெட்வொர்க் இடைமுகங்கள் நிர்வகிக்கும் இயந்திரம் வழங்குகிறது.';
+$lang['wireless_app_name'] = 'வயர்லெஸ் அணுகல் புள்ளி';
+$lang['wireless_bridge'] = 'பாலம்';
+$lang['wireless_channel'] = 'சேனல்';
+$lang['wireless_channel_invalid'] = 'வயர்லெஸ் சேனல் தவறானது.';
+$lang['wireless_mode_invalid'] = 'வயர்லெஸ் முறையில் தவறானது.';
+$lang['wireless_mode'] = 'முறை';
+$lang['wireless_passphrase'] = 'கடவுச்சொற்றொடர்';
+$lang['wireless_radius_secret'] = 'ஆர பகிர்வு இரகசிய';
+$lang['wireless_ssid_invalid'] = 'ஒரு SSID தவறானது.';
+$lang['wireless_interface'] = 'கம்பியில்லா இடைமுகம்';
+$lang['wireless_ssid'] = 'ஒரு SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'டபிள்யூபிஏ உள்கட்டமைப்பு';
+$lang['wireless_wpa_preshared_key'] = 'டபிள்யூபிஏ முன் பகிர்ந்த திறவு';
+$lang['wireless_passphrase_invalid'] = 'வயர்லெஸ் கடவுச்சொற்றொடர் தவறானது';

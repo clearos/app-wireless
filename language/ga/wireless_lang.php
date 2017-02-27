3882e8bc6616d88cbfdef5ecdf170e45bb2b050b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Soláthraíonn an app Pointe Rochtana gan Sreang t-inneall do chumrú agus a bhainistiú comhéadan líonra gan sreang ar an gcóras.';
+$lang['wireless_app_name'] = 'Pointe Gan Sreang Rochtain';
+$lang['wireless_bridge'] = 'droichead';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Is Cainéal Sreang neamhbhailí.';
+$lang['wireless_mode_invalid'] = 'Is modh Sreang neamhbhailí.';
+$lang['wireless_mode'] = 'mód';
+$lang['wireless_passphrase'] = 'Frása Faire';
+$lang['wireless_radius_secret'] = 'RADIUS Rúnda Roinnte';
+$lang['wireless_ssid_invalid'] = 'Is SSID neamhbhailí.';
+$lang['wireless_interface'] = 'Comhéadan Sreang';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'Bonneagar WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Réamh-roinnte Key';
+$lang['wireless_passphrase_invalid'] = 'Is Sreang Frása Faire Neamhbhailí';

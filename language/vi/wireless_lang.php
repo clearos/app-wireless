ee2d6a207e11a6ee65c9a81828550b4d71a18008
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:59:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Các ứng dụng Wireless Access Point cung cấp các công cụ để cấu hình và quản lý các giao diện mạng không dây trên hệ thống.';
+$lang['wireless_app_name'] = 'Điểm truy cập không dây';
+$lang['wireless_bridge'] = 'Cầu';
+$lang['wireless_channel'] = 'Kênh';
+$lang['wireless_channel_invalid'] = 'kênh không dây là không hợp lệ.';
+$lang['wireless_mode_invalid'] = 'chế độ không dây là không hợp lệ.';
+$lang['wireless_mode'] = 'Chế độ';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Chia sẻ bí mật';
+$lang['wireless_ssid_invalid'] = 'SSID là không hợp lệ.';
+$lang['wireless_interface'] = 'Giao diện không dây';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Cơ sở hạ tầng';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Passphrase không dây là không hợp lệ';

430880cdf3fa19c5b9653f83a7498ffd9106d865
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'App a Wireless Aksè Point bay motè a pou konfigirasyon ak jere rezo san fil interfaces sou sistèm nan.';
+$lang['wireless_app_name'] = 'Wireless Aksè Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Chèn';
+$lang['wireless_channel_invalid'] = 'Wireless chanèl se valab.';
+$lang['wireless_mode_invalid'] = 'Wireless mòd se valab.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'pasfraz';
+$lang['wireless_radius_secret'] = 'Reyon Pataje sekrè';
+$lang['wireless_ssid_invalid'] = 'Ssid se valab.';
+$lang['wireless_interface'] = 'Wireless Entèfas';
+$lang['wireless_ssid'] = 'ssid';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Enfrastrikti';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-pataje kle';
+$lang['wireless_passphrase_invalid'] = 'Wireless pasfraz se Envalid';

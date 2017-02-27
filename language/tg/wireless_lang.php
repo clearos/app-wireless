db5e2f36c1842626af32b2a7d20a661ae7ce330f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Дар барнома Point Access бесим барои танзимоти ва идораи интерфейсҳои шабакаи бесим ба системаи таъмин муҳаррики.';
+$lang['wireless_app_name'] = 'Нуқтаи дастра-бесим';
+$lang['wireless_bridge'] = 'пул';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'шабакаи бесим нодуруст аст.';
+$lang['wireless_mode_invalid'] = 'Ҳолати бесим нодуруст аст.';
+$lang['wireless_mode'] = 'ҳолати';
+$lang['wireless_passphrase'] = 'Гузарвожа';
+$lang['wireless_radius_secret'] = 'Радиусаш Махфӣ Shared';
+$lang['wireless_ssid_invalid'] = 'SSID нодуруст аст.';
+$lang['wireless_interface'] = 'Интерфейси бесим';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'Инфраструктура WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Пеш муштарак асосӣ';
+$lang['wireless_passphrase_invalid'] = 'Гузарвожа нодуруст аст бесим';

f9d4dd07802a944bec50808458a55eccbfadf444
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'വയർലെസ് പോയിന്റ് സിസ്റ്റത്തിലെ വയർലെസ്സ് ശൃംഖലാ വിനിമയതലങ്ങൾ ക്രമീകരിയ്ക്കുന്നതിനും കൈകാര്യം എൻജിൻ നൽകുന്നു.';
+$lang['wireless_app_name'] = 'വയർലെസ് പോയിന്റ്';
+$lang['wireless_bridge'] = 'പാലം';
+$lang['wireless_channel'] = 'ചാനൽ';
+$lang['wireless_channel_invalid'] = 'വയർലെസ് ചാനൽ അസാധുവാണ്.';
+$lang['wireless_mode_invalid'] = 'വയർലെസ് മോഡ് അസാധുവാണ്.';
+$lang['wireless_mode'] = 'മോഡ്';
+$lang['wireless_passphrase'] = 'പാസ്ഫ്രെയ്സ്';
+$lang['wireless_radius_secret'] = 'RADIUS പങ്കിട്ട രഹസ്യം';
+$lang['wireless_ssid_invalid'] = 'SSID അസാധുവായ ആണ്.';
+$lang['wireless_interface'] = 'വയർലെസ് ഇന്റര്ഫേസ്';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'ഡബ്ലിയുഇപി';
+$lang['wireless_wpa_infrastructure'] = 'ഡബ്ലിയുപിഎ ഇൻഫ്രാസ്ട്രക്ചർ';
+$lang['wireless_wpa_preshared_key'] = 'WPA മുമ്പ് പങ്കിട്ട കീ';
+$lang['wireless_passphrase_invalid'] = 'വയർലെസ് പാസ്ഫ്രെയ്സ് അസാധുവാണ്';

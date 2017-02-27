1b5efc202b36e475847871dbd9aece0b05683736
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Bevielės prieigos taškas programa suteikia variklį konfigūruoti ir valdyti bevielio tinklo sąsajos sistemoje.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Tiltas';
+$lang['wireless_channel'] = 'kanalas';
+$lang['wireless_channel_invalid'] = 'Bevielio kanalas yra negaliojantis.';
+$lang['wireless_mode_invalid'] = 'Belaidis režimas yra neleistinas.';
+$lang['wireless_mode'] = 'režimas';
+$lang['wireless_passphrase'] = 'slaptažodis';
+$lang['wireless_radius_secret'] = 'RADIUS Bendri paslaptis';
+$lang['wireless_ssid_invalid'] = 'SSID yra negaliojantis.';
+$lang['wireless_interface'] = 'belaidė sąsaja';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA infrastruktūros';
+$lang['wireless_wpa_preshared_key'] = 'WPA bendr raktas';
+$lang['wireless_passphrase_invalid'] = 'Belaidis Slaptafrazė negalioja';

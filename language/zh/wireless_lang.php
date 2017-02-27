290aa506dae19b1624e4f06a693feac53cdbe909
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:49:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = '无线接入点应用程序提供发动机配置和管理系统中的无线网络接口。';
+$lang['wireless_app_name'] = '无线接入点';
+$lang['wireless_bridge'] = '桥';
+$lang['wireless_channel'] = '渠道';
+$lang['wireless_channel_invalid'] = '无线信道是无效的。';
+$lang['wireless_mode_invalid'] = '无线模式是无效的。';
+$lang['wireless_mode'] = '模式';
+$lang['wireless_passphrase'] = '密码';
+$lang['wireless_radius_secret'] = 'RADIUS共享机密';
+$lang['wireless_ssid_invalid'] = 'SSID是无效的。';
+$lang['wireless_interface'] = '无线接口';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA基础设施';
+$lang['wireless_wpa_preshared_key'] = 'WPA预共享密钥';
+$lang['wireless_passphrase_invalid'] = '无线密码是无效的';

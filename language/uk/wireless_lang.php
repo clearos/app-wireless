d79f773fa64d3002712d09e906f0adbd0fb7b300
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:59:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Додаток Бездротова точка доступу забезпечує механізм для налаштування і управління мережевими інтерфейсами бездротової системи.';
+$lang['wireless_app_name'] = 'Бездротова точка доступу';
+$lang['wireless_bridge'] = 'міст';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'Бездротовий канал є недійсним.';
+$lang['wireless_mode_invalid'] = 'Бездротовий режим є неприпустимим.';
+$lang['wireless_mode'] = 'режим';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID є недійсним.';
+$lang['wireless_interface'] = 'Бездротовий інтерфейс';
+$lang['wireless_ssid'] = 'ідентифікатор SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Інфраструктура';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Бездротовий Passphrase неприпустимий';

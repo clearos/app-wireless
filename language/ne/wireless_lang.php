0a9e13cfd2f0c3abb7139576fc824438f8bc13b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'वायरलेस एक्सेस प्वाइन्ट अनुप्रयोग कन्फिगर र सिस्टम मा वायरलेस नेटवर्क इन्टरफेस व्यवस्थापनको लागि इन्जिन प्रदान गर्दछ।';
+$lang['wireless_app_name'] = 'वायरलेस एक्सेस प्वाइन्ट';
+$lang['wireless_bridge'] = 'पुल';
+$lang['wireless_channel'] = 'च्यानल';
+$lang['wireless_channel_invalid'] = 'वायरलेस च्यानल अमान्य छ।';
+$lang['wireless_mode_invalid'] = 'वायरलेस मोड अमान्य छ।';
+$lang['wireless_mode'] = 'मोड';
+$lang['wireless_passphrase'] = 'पासफ्रेज';
+$lang['wireless_radius_secret'] = 'RADIUS साझा रहस्य';
+$lang['wireless_ssid_invalid'] = 'SSID अमान्य छ।';
+$lang['wireless_interface'] = 'वायरलेस इन्टरफेस';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA पूर्वाधार';
+$lang['wireless_wpa_preshared_key'] = 'WPA प्रमुख पूर्व-साझेदारी';
+$lang['wireless_passphrase_invalid'] = 'वायरलेस पासफ्रेज अवैध छ';

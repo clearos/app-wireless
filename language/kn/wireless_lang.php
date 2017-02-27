adc0b260344738e32815644a5cbac1d4e5410a1f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ವೈರ್ಲೆಸ್ ಅಕ್ಸೆಸ್ ಪಾಯಿಂಟ್ ಅಪ್ಲಿಕೇಶನ್ ಸಂರಚಿಸಲು ಮತ್ತು ಗಣಕದಲ್ಲಿ ನಿಸ್ತಂತು ಜಾಲಬಂಧ ಸಂಪರ್ಕಸಾಧನಗಳನ್ನು ನಿರ್ವಹಿಸುವ ಎಂಜಿನ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['wireless_app_name'] = 'ವೈರ್ಲೆಸ್ ಅಕ್ಸೆಸ್ ಪಾಯಿಂಟ್';
+$lang['wireless_bridge'] = 'ಸೇತುವೆ';
+$lang['wireless_channel'] = 'ಚಾನೆಲ್';
+$lang['wireless_channel_invalid'] = 'ವೈರ್ಲೆಸ್ ಚಾನೆಲ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['wireless_mode_invalid'] = 'ವೈರ್ಲೆಸ್ ಕ್ರಮದಲ್ಲಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['wireless_mode'] = 'ಮೋಡ್';
+$lang['wireless_passphrase'] = 'ಪಾಸ್ಫ್ರೇಸ್';
+$lang['wireless_radius_secret'] = 'ತ್ರಿಜ್ಯ ಹಂಚಿಕೆಯ ರಹಸ್ಯ';
+$lang['wireless_ssid_invalid'] = 'SSID ಅಮಾನ್ಯ ಆಗಿದೆ.';
+$lang['wireless_interface'] = 'ವೈರ್ಲೆಸ್ ಇಂಟರ್ಫೇಸ್';
+$lang['wireless_ssid'] = 'SSID ಬದಲಿಸಿ';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'ಡಬ್ಲ್ಯೂಪಿಎ ಇನ್ಫ್ರಾಸ್ಟ್ರಕ್ಚರ್';
+$lang['wireless_wpa_preshared_key'] = 'ಡಬ್ಲ್ಯೂಪಿಎ ಪೂರ್ವ ಹಂಚಿತ ಕೀಲಿ';
+$lang['wireless_passphrase_invalid'] = 'ವೈರ್ಲೆಸ್ ಪಾಸ್ಫ್ರೇಸ್ ಅಮಾನ್ಯವಾಗಿದೆ';

2440f815a556cf2b43ee346eef9cebbda025cf6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:00:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'די וויירליס אַקסעס פּוינט אַפּ גיט די מאָטאָר פֿאַר קאָנפיגורינג און אָנפירונג וויירליס נעץ Interfaces אויף דעם סיסטעם.';
+$lang['wireless_app_name'] = 'ווירעלעסס אַקסעס פּוינט';
+$lang['wireless_bridge'] = 'בריק';
+$lang['wireless_channel'] = 'וועג';
+$lang['wireless_channel_invalid'] = 'ווירעלעסס וועג איז פאַרקריפּלט.';
+$lang['wireless_mode_invalid'] = 'ווירעלעסס מאָדע איז פאַרקריפּלט.';
+$lang['wireless_mode'] = 'מאָדע';
+$lang['wireless_passphrase'] = 'פּאַסספראַסע';
+$lang['wireless_radius_secret'] = 'ראַדיוס שערד סוד';
+$lang['wireless_ssid_invalid'] = 'ססיד איז פאַרקריפּלט.';
+$lang['wireless_interface'] = 'ווירעלעסס צובינד';
+$lang['wireless_ssid'] = 'ססיד';
+$lang['wireless_wep'] = 'וועפּ';
+$lang['wireless_wpa_infrastructure'] = 'וופּאַ Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'וופּאַ Pre-שערד דורכפֿאַל';
+$lang['wireless_passphrase_invalid'] = 'ווירעלעסס פּאַסספראַסע איז אינוואליד';

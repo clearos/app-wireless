0860f8f1a38b00324cd38fea3229929b8797fc3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ການ app Wireless Access Point ໃຫ້ເຄື່ອງຈັກສໍາລັບການກໍາຫນົດຄ່າແລະການຄຸ້ມຄອງການເຊື່ອມຕໍ່ເຄືອຂ່າຍໄຮ້ສາຍໃນລະບົບ.';
+$lang['wireless_app_name'] = 'Access Point Wireless';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'ຊ່ອງທາງ Wireless ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['wireless_mode_invalid'] = 'ຮູບແບບໄຮ້ສາຍທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['wireless_mode'] = 'ຮູບແບບການ';
+$lang['wireless_passphrase'] = 'ລະຫັດຜ່ານ';
+$lang['wireless_radius_secret'] = 'RADIUS ການແລກປ່ຽນຄວາມ';
+$lang['wireless_ssid_invalid'] = 'SSID ເປັນທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['wireless_interface'] = 'Interface Wireless';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'ພື້ນຖານໂຄງລ່າງ WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA Key ທາງສ່ວນຫນ້າຂອງທີ່ໃຊ້ຮ່ວມກັນ';
+$lang['wireless_passphrase_invalid'] = 'Passphrase Wireless ແມ່ນບໍ່ຖືກຕ້ອງ';

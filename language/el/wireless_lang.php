1fb22ef7189f6fdf68ddbf452a4ed06f4b29c6b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Η εφαρμογή Wireless Access Point, παρέχει τον κινητήρα για τη διαμόρφωση και τη διαχείριση των διεπαφών ασύρματου δικτύου στο σύστημα.';
+$lang['wireless_app_name'] = 'Ασύρματο σημείο πρόσβασης';
+$lang['wireless_bridge'] = 'Γέφυρα';
+$lang['wireless_channel'] = 'Κανάλι';
+$lang['wireless_channel_invalid'] = 'Ασύρματο κανάλι είναι άκυρη.';
+$lang['wireless_mode_invalid'] = 'Ασύρματη λειτουργία δεν είναι έγκυρη.';
+$lang['wireless_mode'] = 'Τρόπος';
+$lang['wireless_passphrase'] = 'συνθηματική φράση';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID δεν είναι έγκυρο.';
+$lang['wireless_interface'] = 'ασύρματη διασύνδεση';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Υποδομών';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Ασύρματο Φράση κλειδί δεν είναι έγκυρο';

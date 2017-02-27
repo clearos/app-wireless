5cfea289be1b6a4e7a50ea8aaeecf09c0f316341
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:58:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point app ger motorn för att konfigurera och hantera trådlösa nätverksgränssnitt i systemet.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bro';
+$lang['wireless_channel'] = 'kanalisera';
+$lang['wireless_channel_invalid'] = 'Trådlös kanal är ogiltig.';
+$lang['wireless_mode_invalid'] = 'Trådlöst läge är ogiltig.';
+$lang['wireless_mode'] = 'Läge';
+$lang['wireless_passphrase'] = 'lösenfras';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID är ogiltig.';
+$lang['wireless_interface'] = 'Trådlöst gränssnitt';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA infrastruktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA I förväg delad nyckel';
+$lang['wireless_passphrase_invalid'] = 'Trådlös lösenordsfras är ogiltig';

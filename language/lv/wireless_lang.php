655eae08dabb48ff71759dd7a4f582262ca50224
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Bezvadu Access Point app nodrošina dzinēju konfigurēšanai un pārvaldībai bezvadu tīkla interfeisus sistēmā.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'tilts';
+$lang['wireless_channel'] = 'kanāls';
+$lang['wireless_channel_invalid'] = 'Bezvadu kanāls ir nederīgs.';
+$lang['wireless_mode_invalid'] = 'Bezvadu režīms ir nederīgs.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'parole';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID ir nederīgs.';
+$lang['wireless_interface'] = 'Bezvadu Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktūra';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Bezvadu parole ir nederīga';

118b17209927ec9f3c96ba1f5f99b1fa714368ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:55:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'De Wireless Access Point app biedt de motor voor het configureren en beheren van draadloze netwerk interfaces op het systeem.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Brug';
+$lang['wireless_channel'] = 'Kanaal';
+$lang['wireless_channel_invalid'] = 'Draadloze kanaal is ongeldig.';
+$lang['wireless_mode_invalid'] = 'Wireless mode is ongeldig.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID is ongeldig.';
+$lang['wireless_interface'] = 'draadloze interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructuur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase is ongeldig';

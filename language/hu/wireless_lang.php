e2a329248800e9fa552da512c11e403dc6a29fe9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:51:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'A Wireless Access Point alkalmazás biztosítja a motor konfigurálásához és kezeléséhez, vezeték nélküli hálózati interfészek a rendszerben.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Híd';
+$lang['wireless_channel'] = 'Csatorna';
+$lang['wireless_channel_invalid'] = 'Vezeték nélküli csatorna érvénytelen.';
+$lang['wireless_mode_invalid'] = 'Vezeték nélküli mód érvénytelen.';
+$lang['wireless_mode'] = 'Mód';
+$lang['wireless_passphrase'] = 'Jelmondat';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID érvénytelen.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktúra';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Jelszó érvénytelen';

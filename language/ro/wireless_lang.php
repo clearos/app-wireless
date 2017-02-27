2891ec0150466f44c999ac085ad6b60b8f925652
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:56:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Aplicația Wireless Access Point oferă motorul pentru configurarea și gestionarea interfețelor de rețea fără fir pe sistem.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Pod';
+$lang['wireless_channel'] = 'Canal';
+$lang['wireless_channel_invalid'] = 'canal fără fir este nevalid.';
+$lang['wireless_mode_invalid'] = 'Modul wireless este nevalid.';
+$lang['wireless_mode'] = 'mod';
+$lang['wireless_passphrase'] = 'expresia de acces';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID este nevalid.';
+$lang['wireless_interface'] = 'Interfață fără fir';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructura';
+$lang['wireless_wpa_preshared_key'] = 'WPA pre-partajată Cheie';
+$lang['wireless_passphrase_invalid'] = 'Passphrase fără fir este nevalid';

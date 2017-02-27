fd47841fa834f60cacda6294770e69c43ef8b82d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:49:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The app Opanda zingwe Access Point amapereka injini kwa configuring ndi kusamalira Intaneti polumikizira opanda zingwe pa dongosolo.';
+$lang['wireless_app_name'] = 'Mafoni Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Mafoni njira ili udzakhala osazindikilika.';
+$lang['wireless_mode_invalid'] = 'mode opanda zingwe ndi opanda pake.';
+$lang['wireless_mode'] = 'akafuna';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Utali wozungulira nawo Chinsinsi';
+$lang['wireless_ssid_invalid'] = 'SSID ndi opanda pake.';
+$lang['wireless_interface'] = 'mafoni Chiyankhulo';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA zomangamanga';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-nawo Ofunika';
+$lang['wireless_passphrase_invalid'] = 'Mafoni Passphrase ndi yolakwika';

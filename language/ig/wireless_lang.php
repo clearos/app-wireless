92319a393576857c0d0245532f8ae1bda5928c86
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The Ikuku Access Point ngwa na-enye ndị engine maka configuring na ijikwa ikuku na netwọk interfaces na usoro.';
+$lang['wireless_app_name'] = 'Ikuku Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Ikuku ọwa bụ ghara ịdị irè.';
+$lang['wireless_mode_invalid'] = 'Ikuku mode bụ ghara ịdị irè.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Were n&#39;okirikiri RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID bụ ghara ịdị irè.';
+$lang['wireless_interface'] = 'ikuku Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Nga-akọrọ Key';
+$lang['wireless_passphrase_invalid'] = 'Ikuku Passphrase bụ ghara ịdị irè';

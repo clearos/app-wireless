341cda2935816e91b1721d0f5095d1dd03901c1c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'App Wireless Access Point ofron motor për konfigurimin dhe menaxhimin ndërfaqe të rrjetit pa tel në sistemin.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Urë';
+$lang['wireless_channel'] = 'kanal';
+$lang['wireless_channel_invalid'] = 'Kanali Wireless është i pavlefshëm.';
+$lang['wireless_mode_invalid'] = 'Mënyra Wireless është i pavlefshëm.';
+$lang['wireless_mode'] = 'mënyrë';
+$lang['wireless_passphrase'] = 'passphrase';
+$lang['wireless_radius_secret'] = 'Radius Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID është e pavlefshme.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastruktura';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-përbashkët Key';
+$lang['wireless_passphrase_invalid'] = 'Parafrazën Wireless është i pavlefshëm';

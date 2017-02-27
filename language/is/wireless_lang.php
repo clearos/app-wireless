bc401dd6c5bd64a1695ef0b3f558533c4e2868ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The Wireless Access Point app veitir vél til að stilla og stjórna þráðlausa netviðmóta á kerfinu.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'rás';
+$lang['wireless_channel_invalid'] = 'Wireless rás er ógilt.';
+$lang['wireless_mode_invalid'] = 'Wireless háttur er ógilt.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'aðgangsorð';
+$lang['wireless_radius_secret'] = 'Radíus Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID er ógilt.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Infrastructure';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-hluti lykill';
+$lang['wireless_passphrase_invalid'] = 'Wireless Lykilorð er ógild';

e8e5aa78ff2f2e6fabe6ec03ad4c4950fedd6cee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:57:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'රැහැන් රහිත ප්රවේශ පේදුරු යෙදුම පද්ධතිය මත රැහැන් රහිත ජාල අතුරු මුහුණත් හැඩගැස්වීමෙන් හා කළමනාකරණය සඳහා එන්ජිම සපයයි.';
+$lang['wireless_app_name'] = 'රැහැන් රහිත ප්රවේශ පේදුරු';
+$lang['wireless_bridge'] = 'පාලම';
+$lang['wireless_channel'] = 'නාලිකාව';
+$lang['wireless_channel_invalid'] = 'රැහැන් රහිත නාලිකාව වලංගු නැත.';
+$lang['wireless_mode_invalid'] = 'රැහැන් රහිත මාදිලිය වලංගු නැත.';
+$lang['wireless_mode'] = 'මාදිලිය';
+$lang['wireless_passphrase'] = 'මුරවදනක්';
+$lang['wireless_radius_secret'] = 'අරය සාමූහික රහස්';
+$lang['wireless_ssid_invalid'] = 'SSID වලංගු නැත.';
+$lang['wireless_interface'] = 'රැහැන් රහිත අතුරුමුහුණත';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA යටිතල පහසුකම්';
+$lang['wireless_wpa_preshared_key'] = 'WPA පෙර - නිවේශිත යතුර';
+$lang['wireless_passphrase_invalid'] = 'රැහැන් රහිත මුර වදන වලංගු නොවන වේ';

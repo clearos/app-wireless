2975d3ad6d915e80189b6756094442620daf3b4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:54:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Ny Wireless Access Point fampiharana manome ny maotera sy ny fitantanana ho an&#39;ny Mampiendrika Wireless tambajotra interface eo amin&#39;ny rafitra.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'fantsona';
+$lang['wireless_channel_invalid'] = 'Wireless fantsona dia tsy manan-kery.';
+$lang['wireless_mode_invalid'] = 'Wireless fomba dia tsy manan-kery.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Rayon Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID dia tsy manan-kery.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Fotodrafitrasa';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-nizara Key';
+$lang['wireless_passphrase_invalid'] = 'Wireless Passphrase dia tsy mety';

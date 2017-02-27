a6803c215ca309d612ad6fd6ee41c16365fa12c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:57:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Nu Wireless Access Point app nyadiakeun engine pikeun Konfigurasi sarta menata interfaces jaringan nirkabel dina sistem.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'jambatan';
+$lang['wireless_channel'] = 'Saluran';
+$lang['wireless_channel_invalid'] = 'channel Wireless téh sah.';
+$lang['wireless_mode_invalid'] = 'mode Wireless téh sah.';
+$lang['wireless_mode'] = 'mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Radius Tanjungjaya Rusiah';
+$lang['wireless_ssid_invalid'] = 'SSID téh sah.';
+$lang['wireless_interface'] = 'wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA infrasturktur';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pra-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Passphrase Wireless téh sah';

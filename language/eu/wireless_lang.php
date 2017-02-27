6a908c4ecf5cf3b9db28c7001b7250f745f33456
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:48:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Wireless Access Point Aplikazioa motorra eskaintzen konfigurazioa eta haririk gabeko sare interfazeak kudeatzeko sistemari buruzko da.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'zubia';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Wireless kanal baliogabea da.';
+$lang['wireless_mode_invalid'] = 'Wireless modu baliogabea da.';
+$lang['wireless_mode'] = 'Modu';
+$lang['wireless_passphrase'] = 'Pasa-esaldia';
+$lang['wireless_radius_secret'] = 'RADIUS partekatua Secret';
+$lang['wireless_ssid_invalid'] = 'SSID baliogabea da.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Azpiegitura';
+$lang['wireless_wpa_preshared_key'] = 'WPA Aurrez partek Gakoa';
+$lang['wireless_passphrase_invalid'] = 'Wireless Pasaesaldiak baliogabea da';

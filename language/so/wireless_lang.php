bedf1777f94c9fdf00651eebc7e98e045bb5d5cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:57:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'The app Wireless Access Point waxay bixisaa engine ee configuring iyo maaraynta interfaces network wireless on nidaamka.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'channel Wireless waa baadil.';
+$lang['wireless_mode_invalid'] = 'hab Wireless waa baadil.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'Gacan Wadaago Secret';
+$lang['wireless_ssid_invalid'] = 'SSID waa baadil.';
+$lang['wireless_interface'] = 'wireless Wajahadda';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Kaabayaasha';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-wadaago Key';
+$lang['wireless_passphrase_invalid'] = 'Passphrase Wireless waa baadil';

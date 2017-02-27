84125b54ad76118b431ed69065938c56b8cc3531
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:00:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Insiza Wireless Lendawo Yokungena inikeza injini for ukumisa nokuphatha wireless zenethiwekhi ohlelweni.';
+$lang['wireless_app_name'] = 'Wireless Yokungena';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Isiteshi';
+$lang['wireless_channel_invalid'] = 'isiteshi Wireless ayivumelekile.';
+$lang['wireless_mode_invalid'] = 'Imodi Wireless ayivumelekile.';
+$lang['wireless_mode'] = 'Mode';
+$lang['wireless_passphrase'] = 'Umshwana wokungena';
+$lang['wireless_radius_secret'] = 'RADIUS ngekuhlanganyela Secret';
+$lang['wireless_ssid_invalid'] = 'SSID ayivumelekile.';
+$lang['wireless_interface'] = 'Wireless Interface';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Ingqalasizinda';
+$lang['wireless_wpa_preshared_key'] = 'WPA Oku-wabelane Key';
+$lang['wireless_passphrase_invalid'] = 'Umshwana wokungena Wireless kuyinto engavumelekile';

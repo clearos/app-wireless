706b94d6dd813b05c2cdea4ee8065be4f4b89bb1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:59:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'وائرلیس رسائی نقطہ اے پی پی کی تشکیل اور نظام پر وائرلیس نیٹ ورک انٹرفیس کو منظم کرنے کے لئے انجن فراہم کرتا ہے.';
+$lang['wireless_app_name'] = 'وائرلیس رسائی پوائنٹ';
+$lang['wireless_bridge'] = 'پل';
+$lang['wireless_channel'] = 'چینل';
+$lang['wireless_channel_invalid'] = 'وائرلیس چینل باطل ہے.';
+$lang['wireless_mode_invalid'] = 'وائرلیس موڈ غلط ہے.';
+$lang['wireless_mode'] = 'موڈ';
+$lang['wireless_passphrase'] = 'پاس فریز';
+$lang['wireless_radius_secret'] = 'رداس مشترکہ راز';
+$lang['wireless_ssid_invalid'] = 'SSID غلط ہے.';
+$lang['wireless_interface'] = 'وائرلیس انٹرفیس';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'ڈبلیو پی انفراسٹرکچر';
+$lang['wireless_wpa_preshared_key'] = 'ڈبلیو پی پری مشترکہ کلیدی';
+$lang['wireless_passphrase_invalid'] = 'وائرلیس پاس فریز غلط ہے';

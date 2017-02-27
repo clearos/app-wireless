74a521bbe4d5b0e7ecb55d9c2411d6a84bc9a17c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:56:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Приложение Беспроводная точка доступа обеспечивает механизм для настройки и управления сетевыми интерфейсами беспроводной системы.';
+$lang['wireless_app_name'] = 'Беспроводная точка доступа';
+$lang['wireless_bridge'] = 'Мост';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'Беспроводной канал является недействительным.';
+$lang['wireless_mode_invalid'] = 'Беспроводной режим является недопустимым.';
+$lang['wireless_mode'] = 'Режим';
+$lang['wireless_passphrase'] = 'Passphrase';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID является недействительным.';
+$lang['wireless_interface'] = 'Беспроводной интерфейс';
+$lang['wireless_ssid'] = 'идентификатор SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Инфраструктура';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-Shared Key';
+$lang['wireless_passphrase_invalid'] = 'Беспроводной Passphrase недопустимый';

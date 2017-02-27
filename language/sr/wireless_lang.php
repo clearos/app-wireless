e28c2b0b83a2c3705a1643e09f731d122284d0c3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:56:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Вирелесс Аццесс Поинт, апликација пружа мотор за конфигурисање и управљање бежичне мреже интерфејса на систему.';
+$lang['wireless_app_name'] = 'Вирелесс Аццесс Поинт';
+$lang['wireless_bridge'] = 'мост';
+$lang['wireless_channel'] = 'канал';
+$lang['wireless_channel_invalid'] = 'Бежични канал је неважећи.';
+$lang['wireless_mode_invalid'] = 'Бежични режим је неважећи.';
+$lang['wireless_mode'] = 'начин';
+$lang['wireless_passphrase'] = 'пасспхрасе';
+$lang['wireless_radius_secret'] = 'РАДИУС Заједнички Тајна';
+$lang['wireless_ssid_invalid'] = 'ССИД је неважећи.';
+$lang['wireless_interface'] = 'Бежични интерфејс';
+$lang['wireless_ssid'] = 'ССИД';
+$lang['wireless_wep'] = 'ВЕП';
+$lang['wireless_wpa_infrastructure'] = 'ВПА Инфраструктура';
+$lang['wireless_wpa_preshared_key'] = 'ВПА ПСК кључ';
+$lang['wireless_passphrase_invalid'] = 'Бежични Приступна фраза је неважећи';

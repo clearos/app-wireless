dc181b62da516756791356406a9aa29dfbcd1676
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:59:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Kablosuz Erişim Noktası uygulaması yapılandırma ve sistem üzerinde kablosuz ağ arayüzleri yönetmek için motoru sağlar.';
+$lang['wireless_app_name'] = 'Kablosuz erişim noktası';
+$lang['wireless_bridge'] = 'Köprü';
+$lang['wireless_channel'] = 'Kanal';
+$lang['wireless_channel_invalid'] = 'Kablosuz kanal geçersiz.';
+$lang['wireless_mode_invalid'] = 'Kablosuz modu geçersiz.';
+$lang['wireless_mode'] = 'kip';
+$lang['wireless_passphrase'] = 'Parolası';
+$lang['wireless_radius_secret'] = 'RADIUS Paylaşılan Gizli';
+$lang['wireless_ssid_invalid'] = 'SSID geçersiz.';
+$lang['wireless_interface'] = 'kablosuz Arabirim';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA Altyapı';
+$lang['wireless_wpa_preshared_key'] = 'WPA Ön Paylaşımlı Anahtar';
+$lang['wireless_passphrase_invalid'] = 'Kablosuz Parola geçersiz';

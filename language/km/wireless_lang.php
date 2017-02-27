76501efba8c8f18416566e38147010e90f8cbcc3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:53:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'ចំណុចឥតខ្សែការចូលដំណើរការម៉ាស៊ីនផ្តល់នូវកម្មវិធីសម្រាប់កំណត់រចនាសម្ព័ន្ធចំណុចប្រទាក់បណ្តាញនិងគ្រប់គ្រងឥតខ្សែនៅលើប្រព័ន្ធ។';
+$lang['wireless_app_name'] = 'ចំណុចចូលដំណើរការឥតខ្សែ';
+$lang['wireless_bridge'] = 'ស្ពាន';
+$lang['wireless_channel'] = 'ឆានែល';
+$lang['wireless_channel_invalid'] = 'ឆានែលឥតខ្សែមិនត្រឹមត្រូវ។';
+$lang['wireless_mode_invalid'] = 'របៀបឥតខ្សែមិនត្រឹមត្រូវ។';
+$lang['wireless_mode'] = 'របៀប';
+$lang['wireless_passphrase'] = 'ឃ្លាសម្ងាត់';
+$lang['wireless_radius_secret'] = 'RADIUS សម្ងាត់ដែលបានចែករំលែក';
+$lang['wireless_ssid_invalid'] = 'SSID មិនត្រឹមត្រូវ។';
+$lang['wireless_interface'] = 'ចំណុចប្រទាក់ឥតខ្សែ';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'ហេដ្ឋារចនាសម្ព័ន្ធ WPA';
+$lang['wireless_wpa_preshared_key'] = 'WPA ចែករំលែកគន្លឹះមុន';
+$lang['wireless_passphrase_invalid'] = 'ឃ្លាសម្ងាត់ឥតខ្សែគឺមិនត្រឹមត្រូវ';

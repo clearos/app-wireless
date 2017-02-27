b4663328dc281da2301130d0acdcd5d58ec1e867
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:57:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'Bezdrôtový prístupový bod aplikácia poskytuje motor pre konfiguráciu a správu bezdrôtových sieťových rozhraní v systéme.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Most';
+$lang['wireless_channel'] = 'kanál';
+$lang['wireless_channel_invalid'] = 'Bezdrôtový kanál je neplatný.';
+$lang['wireless_mode_invalid'] = 'Bezdrôtový režim je neplatný.';
+$lang['wireless_mode'] = 'režim';
+$lang['wireless_passphrase'] = 'prístupové heslo';
+$lang['wireless_radius_secret'] = 'RADIUS Shared Secret';
+$lang['wireless_ssid_invalid'] = 'SSID je neplatný.';
+$lang['wireless_interface'] = 'bezdrôtové rozhranie';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA infraštruktúra';
+$lang['wireless_wpa_preshared_key'] = 'WPA Pre-shared Key';
+$lang['wireless_passphrase_invalid'] = 'Bezdrôtové prístupové heslo je neplatné';

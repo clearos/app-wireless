028c1c5f6e5d3142c1fce13df5a0a2a0c031a1f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 11:52:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/wireless_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['wireless_app_description'] = 'უკაბელო დაშვების წერტილი app უზრუნველყოფს ძრავი კონფიგურაციის და მართვის უკაბელო ქსელის ინტერფეისი სისტემა.';
+$lang['wireless_app_name'] = 'Wireless Access Point';
+$lang['wireless_bridge'] = 'Bridge';
+$lang['wireless_channel'] = 'Channel';
+$lang['wireless_channel_invalid'] = 'Wireless არხი არასწორია.';
+$lang['wireless_mode_invalid'] = 'უკაბელო რეჟიმში არასწორია.';
+$lang['wireless_mode'] = 'რეჟიმი';
+$lang['wireless_passphrase'] = 'კოდური ფრაზა';
+$lang['wireless_radius_secret'] = 'რადიუსში საერთო საიდუმლო';
+$lang['wireless_ssid_invalid'] = 'SSID არასწორია.';
+$lang['wireless_interface'] = 'უკაბელო ინტერფეისი';
+$lang['wireless_ssid'] = 'SSID';
+$lang['wireless_wep'] = 'WEP';
+$lang['wireless_wpa_infrastructure'] = 'WPA ინფრასტრუქტურის';
+$lang['wireless_wpa_preshared_key'] = 'WPA წინასწარ გაზიარებული გასაღები';
+$lang['wireless_passphrase_invalid'] = 'Wireless პაროლის არასწორია';
